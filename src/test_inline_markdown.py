aae795aa5a29b9491e2aa63fe7397f8a83d0f164
Nocturnal_Nick <nick@nocturnalnick.com>
2025-05-08 17:55:51 +1200

    Lesson learned

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 
 from textnode import TextNode, TextType
@@ -73,6 +75,78 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_extract_markdown_images(self):
+        # Single image
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([
+            ("image", "https://i.imgur.com/zjjcJKZ.png")
+        ], matches)
+
+        # Multiple images
+        matches = extract_markdown_images(
+            "![alt1](url1) and ![alt2](url2)"
+        )
+        self.assertListEqual([
+            ("alt1", "url1"),
+            ("alt2", "url2")
+        ], matches)
+
+        # No images
+        matches = extract_markdown_images("No images here!")
+        self.assertListEqual([], matches)
+
+        # Empty alt text
+        matches = extract_markdown_images("![](url3)")
+        self.assertListEqual([
+            ("", "url3")
+        ], matches)
+
+        # Image with spaces in alt and url
+        matches = extract_markdown_images("![alt text here](http://example.com/img.png)")
+        self.assertListEqual([
+            ("alt text here", "http://example.com/img.png")
+        ], matches)
+
+    def test_extract_markdown_links(self):
+        # Single link
+        matches = extract_markdown_links(
+            "This is text with a [link](https://www.example.com)"
+        )
+        self.assertListEqual([
+            ("link", "https://www.example.com")
+        ], matches)
+
+        # Multiple links
+        matches = extract_markdown_links(
+            "[one](url1) and [two](url2)"
+        )
+        self.assertListEqual([
+            ("one", "url1"),
+            ("two", "url2")
+        ], matches)
+
+        # No links
+        matches = extract_markdown_links("No links here!")
+        self.assertListEqual([], matches)
+
+        # Link with spaces in anchor and url
+        matches = extract_markdown_links("[anchor text](http://example.com/page)")
+        self.assertListEqual([
+            ("anchor text", "http://example.com/page")
+        ], matches)
+
+        # Should not match images
+        matches = extract_markdown_links("![alt](url)")
+        self.assertListEqual([], matches)
+
+        # Mixed links and images
+        matches = extract_markdown_links("[real link](url) and ![alt](imgurl)")
+        self.assertListEqual([
+            ("real link", "url")
+        ], matches)
 
     def test_delim_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
